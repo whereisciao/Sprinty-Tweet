52024d5d7eaccd7a3cde29e500be7c1003c9b2b1
Steven Chau <steven.chau@mac.com>
2009-08-31 15:19:24 -0500

    Refactored location setup helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def location_setup?
-    if signed_in?
-      current_user.location_enabled?
-    else
-      false
-    end
+    current_user.location_enabled?
   end
 end
